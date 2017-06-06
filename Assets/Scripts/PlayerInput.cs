34219f8ac76308a18a9c152e1b8c39d49059bf67
Rich Barrette <gunnerthemad@hotmail.com>
2017-06-06 10:23:25 -0700

    Initial checkin of skeleton project.  Basic background, simple tile materials, basic lighting and camera setup

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	// FixedUpdate is called after all objects are loaded and once per frame
+	void FixedUpdate()
+	{
+		
+	}
+}
