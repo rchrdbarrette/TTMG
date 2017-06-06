fbe5d4aa98b87541225960c38407f96df0700dbd
Rich Barrette <gunnerthemad@hotmail.com>
2017-06-06 10:50:14 -0700

    created cursor, added playerinput script to control cursor

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -19,6 +19,7 @@ public class PlayerInput : MonoBehaviour {
 	// FixedUpdate is called after all objects are loaded and once per frame
 	void FixedUpdate()
 	{
-		
+		float moveHorizontal = Input.GetAxis("Mouse X");
+		float moveVertical = Input.GetAxis("Mouse Y");
 	}
 }
