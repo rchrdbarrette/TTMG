b761c2a4ef77700ab0c41b25b9531ae30a666f4c
Rich Barrette <gunnerthemad@hotmail.com>
2017-06-09 11:14:27 -0700

    Cursor moves appropriately based on keyboard or controller input

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -6,8 +6,7 @@ using UnityEngine;
 public class PlayerInput : MonoBehaviour
 {
 	private Rigidbody _cursor;
-	private const float CursorSensitivity = (float) 0.01;
-	private Vector3 _cursorDelta, _mouseDelta = new Vector3(0.0f, 0.0f, 0.0f);
+	public int Speed = 10;
 
 	// Use this for initialization
 	void Start()
@@ -18,33 +17,22 @@ public class PlayerInput : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		//if (Input.GetButtonDown("Fire1"))
-		//{
-		//	Debug.Log("Mouse click at: " + Input.mousePosition);
-		//}
+		if (Input.GetButtonDown("Fire1") || Input.GetKeyDown("space"))
+		{
+			Debug.Log("Fire!");
+		}
 	}
 
 	// FixedUpdate is called after all objects are loaded and once per frame
 	void FixedUpdate()
 	{
-		// Vector3 destination = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-		Vector3 destination = transform.position;
-		_mouseDelta = (Input.mousePosition * CursorSensitivity) - _mouseDelta;
-		destination = destination - _cursorDelta;
-		destination.z = -1; // may be unnecessary
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
 
-		if (Input.GetButtonDown("Fire1"))
-		{
-			var debugging = String.Format("position: {0} delta: {1} / mouse: {2} mdelta: {3}", _cursor.position, _cursorDelta, Input.mousePosition, _mouseDelta);
-			Debug.Log(debugging);
-		}
-
-		// _cursor.velocity = (transform.position - _cursorDelta) * CursorSensitivity;
-		_cursor.MovePosition(destination);
-
-		// rigidbody.velocity = ((transform.right * mouseMovement.x) + (transform.forward * mouseMovement.y)) / Time.deltaTime;
-		// cursor.velocity = ((transform.right * destination.x) + (transform.forward * destination.y)) / Time.deltaTime;
-		// rigidbody.rotation = Quaternion.Euler(rigidbody.rotation.eulerAngles + new Vector3(0f, rotationSpeed * Input.GetAxis("Mouse X"), 0f));
-		_cursorDelta = _mouseDelta * CursorSensitivity;
+		// move the cursor around by keyboard or controller
+		Vector3 position = transform.position;
+		position.x += moveHorizontal * Speed * Time.deltaTime;
+		position.y += moveVertical * Speed * Time.deltaTime;
+		transform.position = position;
 	}
 }
